01dc36dd86a6d55d81ebada072e1eee3f45f0caf
David Kagan <david.kagan07@gmail.com>
2024-09-01 20:54:30 -0400

    feat: add display
    Fow now, the display is only black. This is to get initial setup for the
    gameplay loop.

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pf = pygame.init()
+    if pf[1] > 0:
+        print("failed pygame.init()")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        # This checks if the user has closed the window
+        # AKA makes the X (close) button work
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(color=pygame.Color(0, 0, 0))
+        pygame.display.flip()
+
 
 # ensures main() is only called when this file is run directly
 if __name__ == "__main__":
