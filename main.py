d059bb1eb745e4a182327ddc96e0ad72eade0a12
David Kagan <david.kagan07@gmail.com>
2024-09-01 20:42:20 -0400

    feat: implement constants file and import

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
+from constants import *
 
 
 def main():
     """main function is our main function"""
 
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 # ensures main() is only called when this file is run directly
