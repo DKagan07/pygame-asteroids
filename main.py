786519bdb410c24138f238252b5a94090c4f80b8
David Kagan <david.kagan07@gmail.com>
2024-09-01 20:35:08 -0400

    Initial commit and venv setup
    to set up venv, use the command `source venv/bin/activate` after the command `python3 -m venv venv` to create the dir

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+
+def main():
+    """main function is our main function"""
+
+    print("Starting asteroids!")
+
+
+# ensures main() is only called when this file is run directly
+if __name__ == "__main__":
+    main()
